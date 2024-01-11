6ae69e4650828104466bf4dd9694e99d97e3fbd9
Tomi Sugiarto <sugiartomi@gmail.com>
2024-01-11 15:12:04 +0700

    testing route /

--- router/router.js
+++ router/router.js
@@ -3,6 +3,7 @@ const UserController = require("../controllers/UserController");
 const Authentication = require("../helpers/Authentication");
 const Authorization = require('../helpers/Authorization')
 
+router.get("/", ((req, res) => res.send("holla")))
 router.post("/login", UserController.login)
 
 router.post("/register", UserController.register)
